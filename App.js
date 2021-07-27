50a3defbdf26831bf1f9bee359a71e6c334a1559
Andrii Melnyk <melnik.andrey@gmail.com>
2021-07-27 12:35:12 -0400

    adding SQLlite, local storage logic

--- App.js
+++ App.js
@@ -19,6 +19,16 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import ReduxThunk from "redux-thunk";
 import placesReducer from "./src/services/store/reducers/places-reducers";
+import { init } from "./src/services/helper/db";
+
+init()
+  .then(() => {
+    console.log("Initialized database");
+  })
+  .catch((err) => {
+    console.log("Initialized db failed");
+    console.log(err);
+  });
 
 const rootReducer = combineReducers({
   places: placesReducer,
